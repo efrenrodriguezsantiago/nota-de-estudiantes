0de951cd0b15b6f8dc41d02bb8349b12dc6d9043
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-17 21:32:08 -0500

    llenando un arreglo

---
+++ src/arreglo/llenando_arreglo.java
@@ -0,0 +1,32 @@
+package arreglo;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class llenando_arreglo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		// llenar arreglo
+		int elemento;
+		
+		elemento =Integer.parseInt(JOptionPane.showInputDialog(null,"digite la cantidad de elementos"));
+		
+		char[] letra = new char[elemento];
+		
+		System.out.println(" Digite los elementos del arreglo: ");
+		for(int i=0;i<=elemento;i++) {
+				System.out.print((i+1)+" digite un caracter " );//SEUTILIZA I + 1 PARA NO EMPEZAR DE 0 SI NO APARTIR DE 1
+				letra[i] = entrada.next().charAt(0);
+		}
+		
+		
+		System.out.println(" los caracteres del arreglo son: ");
+		for(int i=0;i<elemento;i++) {
+			System.out.print(letra[i]+" ");
+		}	
+	}
+
+}
