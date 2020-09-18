bc93b3459f16de45d8aae0884163e4216f985d05
CONTA-47 <CONTA-47@DESKTOP-RQ7SNBR>
2020-09-18 03:01:26 -0500

    nota de estudiantes

---
+++ src/arreglo/nota_estudiante.java
@@ -0,0 +1,19 @@
+package arreglo;
+
+import java.util.Scanner;
+
+public class nota_estudiante {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		// CADA GRUPO O CLASE ESTA COMPUESTA POR 5 ALUMNOS 
+		//SE PIDE LEER LAS NOTAS DE PRIMERO SEGUNDO Y TERCER TRIMESTRE DE UNN GRUPO
+		//DEBEMOS MOSTRAR AL FINAL LA NOTA MEDIA DEL GRUPO EN CADA TRIMESTRE
+		//Y DE LA MEDIA DEL ALUMNO QUE SE ENCUENTRA EN LA POSICION N(N SE LEE POR TECLADO)
+		
+		
+		
+		
+	}
+
+}
